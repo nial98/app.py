3a79c07768a2fc39ede7fb42a07f8268f78b3217
Nial Alwash <nial.alwash@wustl.edu>
2017-05-02 18:32:03 +0000

    post_motion.py created online with Bitbucket

---
+++ post_motion.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+import urllib3
+import certifi
+import json
+
+GPIO.setmode(GPIO.BCM)
+PIR_PIN=7
+GPIO.setup(PIR_PIN, GPIO.IN)
+sec = 10
+try:   
+    print "PIR Module Test (CTRL+C to exit)"
+    time.sleep(2)
+    print "ready"
+    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    while True:
+            if GPIO.input(PIR_PIN):
+                    sec = 0
+                    r = http.request('GET', '52.60.213.136/occupied')
+                    
+                    if not r.status == 200:
+                            print 'Server unreachable: ' + str(r.status)
+            else:
+                    sec += 1
+                    r = http.request('GET' , '52.60.213.136/unoccupied')
+                    
+                    if not r.status == 200:
+                            print 'Server unreachable: ' + str(r.status)
+            
+            time.sleep(1)
+            if sec >= 10:
+                    print "not occupied"
+                    print "time since motion detected", sec, "seconds"
+            else:
+                    print "occupied"
+except KeyboardInterrupt:
+        print "quit"
+        GPIO.cleanup()
